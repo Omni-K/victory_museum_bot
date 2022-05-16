9f180ab26fca8fc5378507d9660585aa5e720298
Fedor Gornostaev <102575921+P-or-G@users.noreply.github.com>
2022-05-16 05:46:51 +0300

    Update keyboards_gor.py

--- keyboards_gor.py
+++ keyboards_gor.py
@@ -4,24 +4,22 @@ from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButt
 
 nokb = ReplyKeyboardRemove()
 
-s_back = InlineKeyboardButton('Назад', callback_data='menu')
+s_back = InlineKeyboardButton('Вернуться в меню', callback_data='menu')
 st_but = InlineKeyboardMarkup(row_width=1)
 mn_but = InlineKeyboardButton('Открыть меню', callback_data='menu')
 st_but.add(mn_but)
 
 user_menu = InlineKeyboardMarkup(row_width=1)
-expo_info_btn = InlineKeyboardButton('Часы работы и стоимость', callback_data='expo_info_btn')
+mus_info = InlineKeyboardButton('О музее', callback_data='mus_info')
+expo_info_btn = InlineKeyboardButton('Что посетить?', callback_data='expo_info_btn')
 social_btn = InlineKeyboardButton('Мы в соцсетях', callback_data='social_btn')
-subscribe_btn = InlineKeyboardButton('Рассылка', callback_data='user_subscribe')
-buytiket_btn = InlineKeyboardButton('Купить билеты', callback_data='buytikets')
-podvig_btn = InlineKeyboardButton('Экспозиция "Подвиг народа"', callback_data='podvig_btn')
-kino_btn = InlineKeyboardButton('Кинотеатр музея', callback_data='kino_btn')
-user_menu.add(expo_info_btn,
+subscribe_btn = InlineKeyboardButton('Новости музея', callback_data='user_subscribe')
+siblings_btn = InlineKeyboardButton('Узнать о родственниках-ветеранах', callback_data='siblings_btn')
+user_menu.add(mus_info,
+              expo_info_btn,
               social_btn,
               subscribe_btn,
-              buytiket_btn,
-              podvig_btn,
-              kino_btn,
+              siblings_btn
               )
 
 admin_menu = InlineKeyboardMarkup(row_width=1)
@@ -39,10 +37,12 @@ buy_in_tg = InlineKeyboardButton('Купить в телеграм', callback_da
 buy_on_site = InlineKeyboardButton('Купить на сайте', url='https://tickets.victorymuseum.ru/ru/#id=1')
 tikets_kb.add(buy_in_tg, buy_on_site, s_back)
 
-podvig_kb = InlineKeyboardMarkup(row_width=1)
-url_podvig = InlineKeyboardButton('Прочитать об экспозиции',
-                                  url='https://victorymuseum.ru/excursions/podvig-naroda/podvig-naroda/')
-podvig_kb.add(url_podvig, s_back)
+
+expo_kb = InlineKeyboardMarkup(row_width=1)
+expos_link = InlineKeyboardButton('Наши выставки', callback_data='expos_link')
+kino_btn = InlineKeyboardButton('Кинотеатр музея', callback_data='kino_btn')
+buytiket_btn = InlineKeyboardButton('Купить билеты', callback_data='buytikets')
+expo_kb.add(expos_link, buytiket_btn, kino_btn, s_back)
 
 soc_kb = InlineKeyboardMarkup(row_width=1)
 ut = InlineKeyboardButton('Наш Youtube', url='https://www.youtube.com/channel/UCfJd9RT4pg_CckntWhlhH0w')
